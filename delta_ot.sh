bb08bd0cdc0c970679e55c748146be2a3d23f009
fc-cr <46376625+fc-cr@users.noreply.github.com>
2020-02-18 17:16:15 +0100

    Initial commit

---
+++ delta_ot.sh
@@ -0,0 +1,24 @@
+_tp_bin="/usr/sap/trans/bin"
+_log_mail="/usr/sap/trans/bin/Script/liste_OT.version"
+_tp_cmd="/usr/sap/${SAPSYSTEMNAME}/SYS/exe/run/tp"
+_tp_profile="$_tp_bin/TP_DOMAIN_SD1.PFL"
+
+
+input=""
+while IFS= read -r line
+do
+	echo "Transport de l'OT $line sur $SAPSYSTEMENAME"
+	tp addtobuffer $line $SAPSYSTEMENAME pf=$_tp_profile
+	_CR=$?
+	if [[ $_CR -gt 7 ]];then
+		echo "$line en erreur $_CR sur $SAPSYSTEMENAME"
+		exit $_CR
+	fi
+	
+	tp import $line $_SID u1234 pf=$_tp_profile
+	
+	if [[ $_CR -gt 7 ]];then
+		echo "$line en erreur $_CR sur $SAPSYSTEMENAME"
+		exit $_CR
+	fi
+done < "$input"
