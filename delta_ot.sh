b159c0acb943f2b492bb11992b71aaeeef4797c1
fc-cr <46376625+fc-cr@users.noreply.github.com>
2020-02-18 17:35:08 +0100

    Update delta_ot.sh

--- delta_ot.sh
+++ delta_ot.sh
@@ -3,21 +3,28 @@ _tp_cmd="/usr/sap/${SAPSYSTEMNAME}/SYS/exe/run/tp"
 _tp_profile="$_tp_bin/TP_DOMAIN_SD1.PFL"
 
 
-input=""
+input="test"
 while IFS= read -r line
 do
-	echo "Transport de l'OT $line sur $SAPSYSTEMENAME"
-	tp addtobuffer $line $SAPSYSTEMENAME pf=$_tp_profile
-	_CR=$?
-	if [[ $_CR -gt 7 ]];then
-		echo "$line en erreur $_CR sur $SAPSYSTEMENAME"
-		exit $_CR
-	fi
-	
-	tp import $line $_SID u1234 pf=$_tp_profile
-	
-	if [[ $_CR -gt 7 ]];then
-		echo "$line en erreur $_CR sur $SAPSYSTEMENAME"
-		exit $_CR
-	fi
+    _OT=$(echo $line | awk -F ';' '{print $1}' | sed 's/ //g')
+    _CRatt=$(echo $line | awk -F ';' '{print $2}'| sed 's/ //g')
+    echo "Transport de l'OT $_OT sur $SAPSYSTEMENAME avec CR attendu $_CRatt"
+    tp addtobuffer $_OT $SAPSYSTEMENAME pf=$_tp_profile
+    _CR=$?
+    if [[ $_CR -gt 7 ]];then
+        if [[ $_CR -ne _CRatt ]];then
+            echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
+            exit $_CR
+        fi
+    fi
+
+    tp import $_OT $_SID u1234 pf=$_tp_profile
+
+    if [[ $_CR -gt 7 ]];then
+        if [[ $_CR -ne _CRatt ]];then
+            echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
+            exit $_CR
+        fi
+    fi
+    echo "$_OT transporté avec CR $_CR"
 done < "$input"
