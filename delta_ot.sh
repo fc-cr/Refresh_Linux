984ef3f210d407170275170084104b3d33ce4298
fc-cr <46376625+fc-cr@users.noreply.github.com>
2020-02-19 08:28:16 +0100

    Update delta_ot.sh

--- delta_ot.sh
+++ delta_ot.sh
@@ -1,29 +1,51 @@
+#*************************
+#
+#   Script de transport d'OT
+#
+#   Format du fichier:
+#   <Num de l'ot>;<CR attendu>
+#    SIDK000000;0
+#
+#*************************
+
+
+
+read -p "Quel est le nom du fichier : " input
+read -p "Quel est le nom du controleur de domaine : " _DC
+
 _tp_bin="/usr/sap/trans/bin"
 _tp_cmd="/usr/sap/${SAPSYSTEMNAME}/SYS/exe/run/tp"
-_tp_profile="$_tp_bin/TP_DOMAIN_SD1.PFL"
+_tp_profile="$_tp_bin/TP_DOMAIN_$_DC.PFL"
 
-
-input="test"
 while IFS= read -r line
 do
     _OT=$(echo $line | awk -F ';' '{print $1}' | sed 's/ //g')
     _CRatt=$(echo $line | awk -F ';' '{print $2}'| sed 's/ //g')
-    echo "Transport de l'OT $_OT sur $SAPSYSTEMENAME avec CR attendu $_CRatt"
+    if [[ -z _$CRatt ]];then
+        echo "Transport de l'OT $_OT sur $SAPSYSTEMENAME"
+    else
+        echo "Transport de l'OT $_OT sur $SAPSYSTEMENAME avec CR attendu $_CRatt"
+    fi
+
     tp addtobuffer $_OT $SAPSYSTEMENAME pf=$_tp_profile
     _CR=$?
     if [[ $_CR -gt 7 ]];then
-        if [[ $_CR -ne _CRatt ]];then
-            echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
-            exit $_CR
+        if [[ ! -z _$CRatt ]];then
+            if [[ $_CR -ne _CRatt ]];then
+                echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
+                exit $_CR
+            fi
         fi
     fi
 
     tp import $_OT $_SID u1234 pf=$_tp_profile
 
     if [[ $_CR -gt 7 ]];then
-        if [[ $_CR -ne _CRatt ]];then
-            echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
-            exit $_CR
+        if [[ ! -z _$CRatt ]];then
+            if [[ $_CR -ne _CRatt ]];then
+                echo "$_OT en erreur $_CR sur $SAPSYSTEMENAME"
+                exit $_CR
+            fi
         fi
     fi
     echo "$_OT transporté avec CR $_CR"
